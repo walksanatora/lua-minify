f716c1b95346bb6c66a964d221a9b172d1161390
walksanatora <walkerffo22@gmail.com>
2023-01-13 23:10:48 -0800

    one last test, probally wont work though

--- main.sh
+++ main.sh
@@ -4,23 +4,23 @@ if [ -n "$GITHUB_WORKSPACE" ]
     cd "$GITHUB_WORKSPACE"
     echo $argv[1]
     for file in (echo $argv[1] | xargs find)
-        echo "$file"
-        luamin -f "$file" > "$file".tmp
+        echo "minify: $file"
         if luamin -f "$file" > "$file".tmp
-            echo "Lua valid, renaming $file.tmp -> $file"
+            echo "valid, renaming $file.tmp -> $file"
             if not set -q DRY_RUN then
                 mv "$file.tmp" "$file"
             else
                 echo "DRY RUN, file not changed"
             end
         else
-            echo "FILE IS NOT LUA"
+            echo "luamin failed"
             cat "$file.tmp"
             rm "$file.tmp"
         end
     end
 else
     echo "Not running in GH actions"
+    bash
 end
 
 exit 0
